d7b19c80bff33ed6bf85fb9c93bb90e3765364fb
수안 <suan@MacBook-Pro-cua-suan.local>
2024-06-19 16:26:24 +0900

    first commit

---
+++ hellojava.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class hellojava {
+    public static void main(String[] args) {
+        //출력
+        System.out.println("hello java!!");
+
+        // 입력 받고 출력
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine());
+        //
+    }
+
+}
